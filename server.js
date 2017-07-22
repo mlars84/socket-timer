1f3c4fe440cdc3eb95884be2eeb9615a516f8396
Matt Larson <matt.a.larson@gmail.com>
2017-07-22 12:02:05 -0500

    added server.js for socket.io and running on 8000

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-const io = require('socket.io');
+const io = require('socket.io')();
+
+io.on('connection', (client) => {
+  // start emitting events to the client
+
+});
+
+const port = 8000;
+io.listen(port);
+console.log('listening on port', port);
