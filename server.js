d46897bf4bc9de30cd0c4c02aa8e678ccc006bdd
Matt Larson <matt.a.larson@gmail.com>
2017-07-22 13:12:30 -0500

    finished walkthrough, Failed to Compile

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const io = require('socket.io')();
 
 io.on('connection', (client) => {
-  // start emitting events to the client
-
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
 });
 
 const port = 8000;
 io.listen(port);
-console.log('listening on port', port);
+console.log('listening on port ', port);
--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { subscribeToTimer } from './api';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        This is the timer value: {this.state.timestamp}
         </p>
-      </div>
-    );
+        </div>
+      );
   }
 }
+
+constructor(props) {
+  super(props);
+  subscribeToTimer((err, timestamp) => this.setState({
+    timestamp
+  }));
+}
+
+state = {
+  timestamp: 'no timestamp yet'
+};
 
 export default App;
